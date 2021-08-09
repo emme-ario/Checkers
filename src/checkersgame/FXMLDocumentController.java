76988d81178c348c6c653c9852a891323e3a06a5
Matteo Mariottini <matteomariottini9@gmail.com>
2021-08-09 18:06:35 +0200

    Upload first GUI prototype.

--- src/checkersgame/FXMLDocumentController.java
+++ src/checkersgame/FXMLDocumentController.java
@@ -7,24 +7,18 @@ package checkersgame;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author Mariottini
  */
 public class FXMLDocumentController implements Initializable {
-    
-    private Label label;
-    
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
